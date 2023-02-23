7346ca9b9aee22068d30b9234f6ef86a0261a9f0
Leonardo <leo.ribeiros2014@gmail.com>
2023-02-23 08:02:49 -0300

    Correção

--- pratica01/src/controle/Conexao.java
+++ pratica01/src/controle/Conexao.java
@@ -5,40 +5,43 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
+
 	private static Connection conexao;
 	private static Conexao instancia;
-	private static final String DATABASE = "leo";
+	private static final String DATABASE = "jdbc";
 	private static final String USER = "root";
 	private static final String PSW = "aluno";
-	
-	private Conexao() {	
-	}	
-	
-	public static Conexao getInstancia() {
-		if(instancia == null) {
+
+	private Conexao() {
+
+	}
+
+	public static Conexao getInstacia() {
+		if (instancia == null) {
 			instancia = new Conexao();
 		}
 		return instancia;
 	}
-	
-	public Connection conectar() {
+
+	public static Connection conectar() {
 		try {
-			conexao = DriverManager.getConnection("jdbc:mysql://localhost/"+ DATABASE + "?serverTimezone=UTC", USER, PSW);
+			conexao = DriverManager.getConnection("jdbc:mysql://localhost/" + DATABASE + "?serverTimezone=UTC", USER,
+					PSW);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return conexao;
+
 	}
+
 	public boolean fecharConexao() {
-		try
-		{
-		conexao.close();
-		}catch (SQLException e) {
+		try {
+			conexao.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
-		
 	}
-	
+
 }
--- pratica01/src/controle/PessoaDAO.java
+++ pratica01/src/controle/PessoaDAO.java
@@ -1,73 +1,77 @@
+
 package controle;
 
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import modelo.Pessoa;
 
 public class PessoaDAO {
+	
 	private Conexao con;
-	public boolean inserir(Pessoa p) {
-		//Instanciar conexão//
-		con = Conexao.getInstancia();
-		
-		//Abrir conexão//
+	
+	public boolean Inserir(Pessoa pessoa) {
+		con = Conexao.getInstacia();
 		Connection c = con.conectar();
 		try {
-			String query = "INSERT INTO pessoa (cpf,nome) VALUES (?, ?);";
-		PreparedStatement stm = c.prepareStatement(null);
-		stm.setInt(1, 12345);
-		stm.setString(2, "leonardo");
-		int valida = stm.executeUpdate();
+			String query = "Insert INto pessoa(cpf, nome)values(?,?);";
+			PreparedStatement stm = c.prepareStatement(query);
+			stm.setLong(1, 123);
+			stm.setString(2, "Elieie");
+			
+			int valida = stm.executeUpdate();
 		
-		}catch (SQLException e){
+		
+		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
-		//Fechar conexão//
+		con.fecharConexao();
+		return false;			
+	}
+	
+	public boolean atualizar(Pessoa pessoa) {
+		con = Conexao.getInstacia();
+		con.conectar();
 		con.fecharConexao();
 		return false;
 	}
-
-	public boolean atualizar(Pessoa p) {
-		
-		return false;
-	}
-	public boolean deletar(Pessoa p) {
-		
+	
+	public boolean deleter(Pessoa pessoa){
+		con = Conexao.getInstacia();
+		con.conectar();
+		con.fecharConexao();
 		return false;
 	}
 	
-	public ArrayList<Pessoa> listarPessoas(){
-		ArrayList <Pessoa> pessoas = new ArrayList<>();
-		//Instanciar conexão//
-				con = Conexao.getInstancia();
+	public ArrayList<Pessoa>listarPessoas(){
+		ArrayList<Pessoa> pessoas = new ArrayList<>();
+		con = Conexao.getInstacia();
+		Connection c = con.conectar();
+		try {
+			Statement stm = c.createStatement();
+			String query = "Select * from pessoa";
+			ResultSet rs = stm.executeQuery(query);
+			while(rs.next()) {
+				int cpf = rs.getInt("cpf");
+				String nome = rs.getString("nome");
+				Pessoa p= new Pessoa();
+				p.setCpf(cpf);
+				p.setNome(nome);
+				pessoas.add(p);
 				
-				//Abrir conexão//
-				Connection c = con.conectar();
-				try {
-					Statement stm = c.createStatement();
-					String query = "SELECT *FROM pessoa";
-					ResultSet rs = stm.executeQuery(query);
-					while (rs.next()){
-						int id = rs.getInt("cpf");
-						String nome = rs.getString("nome");
-						Pessoa p = new Pessoa();
-						p.setCpf(cpf);
-						p.setNome(nome);
-						pessoas.add(p);
-					}
-				}catch (SQLException e){
-					e.printStackTrace();
-				}
-				
-				//Fechar conexão//
-				con.fecharConexao();
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		con.fecharConexao();
 		return null;
 	}
+		
+	
+	
+
 }
